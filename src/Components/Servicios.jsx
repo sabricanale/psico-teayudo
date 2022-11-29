868912cffdbecd155a35f1255288c297cf6d925e
Sabri <sabrii.canale@gmail.com>
2022-11-29 00:47:16 -0300

    CAmbios

--- src/Components/Servicios.jsx
+++ src/Components/Servicios.jsx
@@ -4,59 +4,63 @@ const Servicios = () => {
         <div>
             <div className="container">
             <h1 className="text-center p-5">¿Cómo podemos ayudarte?</h1>
-                <div className="row">
-                    <div className="col-md-12 text-center fs-5">
-                        <p>Todos atravesamos momentos difíciles, desde inseguridades personales hasta problemas en nuestras relaciones. Te invitamos a que empieces a transformar tu vida en terapia con un psicólogo online o cerca de tu zona</p>
+                                   
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="col-md-12 text-center fs-5" id="intro">
+                    <p>Todos atravesamos momentos difíciles, desde inseguridades personales hasta problemas en nuestras relaciones. Te invitamos a que empieces a transformar tu vida en terapia con un psicólogo online o cerca de tu zona</p>
+                </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="Images/RelacionesPersonales.jpg" className="card-img-top" alt="Relaciones personales"/>
+                    <div className="card-body">
+                        <p className="text-center fs-4 m-4">Relaciones personales</p>
                     </div>
-                    
-                    <div className="col-md-3 m-5">
-                        <div className="text-center">
-                            <img src="Images/RelacionesPersonales.jpg" alt="Relaciones personales" width={300}/>
-                            <h6 className="p-2 ">Relaciones personales</h6>
-                        </div>
-                    </div>
-                    
-                    <div className="col-md-3 m-5">
-                        <div className="text-center">
-                            <img src="Images/Miedos.jpg" alt="Miedos" width={220}/>
-                            <h6 className="p-3">Miedos</h6>
-                        </div>
-                    </div>
-
-                    <div className="col-md-3 p-5">
-                        <div className="text-center">
-                            <img src="Images/Estres.jpg" alt="Estrés y cansancio" width={300}/>
-                            <h6 className="m-3">Estrés y cansancio</h6>
-                        </div>
-                    </div>
-
-                    <div className="col-md-4">
-                        <div className="text-center">
-                            <img src="Images/Inseguridades" alt="Toma de decisiones" width={300}/>
-                            <h6 className="p-2">Toma de decisiones</h6>
-                        </div>
-                    </div>
-                    
-                    <div className="col-md-3 m-5 pt-3">
-                        <div className="text-center">
-                            <img src="Images/Animo.jpg" alt="Ánimo" width={220}/>
-                            <h6 className="p-3">Ánimo</h6>
-                        </div>
-                    </div>
-
-                    <div className="col-md-3 m-4">
-                        <div className="text-center">
-                            <img src="Images/Autoestima.jpg" alt="Autoestima" width={300}/>
-                            <h6 className="p-3">Autoestima e inseguridades</h6>
-                        </div>
-                    </div>
-
-            
                 </div>
             </div>
-                
+            <div className="col">
+                <div className="card h-100">
+                    <img src="Images/Miedos.jpg" className="card-img-top" alt="Miedos"/>
+                    <div className="card-body">
+                        <p className="text-center fs-4 m-4">Miedos</p>
+                    </div>
+                </div>
             </div>
-            
+            <div className="col">
+                <div className="card h-100">
+                    <img src="Images/Estres.jpg" className="card-img-top" alt="Estrés y cansancio"/>
+                    <div className="card-body">
+                        <p className="text-center fs-4 m-4">Estrés y cansancio</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="Images/Inseguridades" className="card-img-top" alt="Toma de decisiones"/>
+                    <div className="card-body">
+                        <p className="text-center fs-4 m-4">Toma de decisiones</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="Images/Animo.jpg" className="card-img-top" alt="Ánimo"/>
+                    <div className="card-body">
+                        <p className="text-center fs-4 m-4">Ánimo</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="Images/Autoestima.jpg" className="card-img-top" alt="Autoestima e inseguridades"/>
+                    <div className="card-body">
+                        <p className="text-center fs-4 m-4">Autoestima e inseguridades</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</div>
+      
     )
 }
 
