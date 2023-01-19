84a7d4de4d8c621e4ded6280edcd15f32a8f8b22
Sabri <sabrii.canale@gmail.com>
2023-01-19 00:41:16 -0300

    Modificacion 1-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import Carrousel from './Components/Carousel';
 import Encabezado from './Components/Encabezado';
@@ -6,19 +5,21 @@ import Footer from './Components/Footer';
 import Modalidad from './Components/Modalidad';
 import Servicios from './Components/Servicios';
 import Somos from './Components/Somos';
+import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
     
       <div>
-
+        <BrowserRouter>
           <Encabezado/>
           <Carrousel/>
           <Somos/>
           <Servicios/> 
           <Modalidad/>             
           <Footer/>
-
+        </BrowserRouter>
         </div>
 
     
--- src/Components/Somos.jsx
+++ src/Components/Somos.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom"
+
 
 const Somos = () => {
     return(
       <div>
         <div className="container">
-         <Link to={"/"}>
-              <h1 className="animate__animated animate__fadeInDown text-center p-3 fs-1 mt-2 fw-bold" id="Nosotras">¿Quiénes somos?</h1>
-         </Link> 
+          <h1 className="animate__animated animate__fadeInDown text-center p-3 fs-1 mt-2 fw-bold" id="Nosotras">¿Quiénes somos?</h1>
+        
           <div className="row">
             <div className="col-md-12">
               <p className="fs-5 m-3 text-center"><span className="text-dark fw-bold">PsicoTeAyudo</span> pretende tener una participación activa en aquellas personas que están atravesando una situación de vulnerabilidad, paralizadas ante una problemática determinada y buscan ayuda terapéutica. Nuestro equipo cuenta con las herramientas terapéuticas para adecuarlas a cada paciente, atendiendo la circunstancia que esté atravesando.</p>
@@ -53,110 +52,7 @@ const Somos = () => {
           </div>
         </div>
         
-              
-{/*
 
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-4">
-                      <p className="fs-5 p-3"><span className="text-dark fw-bold">PsicoTeAyudo</span> pretende tener una participación activa en aquellas personas que están atravesando una situación de vulnerabilidad, paralizadas ante una problemática determinada y buscan ayuda terapéutica. Nuestro equipo cuenta con las herramientas terapéuticas para adecuarlas a cada paciente, atendiendo la circunstancia que esté atravesando.</p>
-                  </div>
-                  <div className="card placeholder-glow">
-                    <div className="text-center">
-                      <img src="./Images/Marilyn.png" className="img-fluid rounded-start" alt="Psicóloga Marilyn Gómez" width={200}/>
-                    </div>
-                      <div className="card-body col-md-">
-                        <h3 className="card-title">Lic. Marilyn Gómez</h3>
-                        <h5 className="card-text fw-bold">PSICÓLOGA </h5>
-                        <p>M.P. 12788</p>
-                        <p>Atención online, presencial y domiciliaria</p>          
-                        <a href="/" className="animate__animated animate__swing btn">Más info</a>
-                      </div>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-
-            
-
-
-
-
-*/}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  {/*
-  <div className="row justify-content-around p-4 mx-auto">
-            <div className="col-xl-4 col-lg-5 col-md-5 col-sm-6" id="tarjeta">
-              <div className="card">
-                  <div className="head">
-                    <div className="circle">
-                      <div className="img">
-                        <img className="img-fluid" src="Images/Marilyn.png" alt="Lic. Marilyn Gómez"/>
-                      </div>
-                    </div>
-                  </div>
-                <div className="description">
-                  <h3>Lic. Marilyn Gómez</h3>
-                  <p className="fw-bold">PSICÓLOGA</p>
-                  <p>M.P. 12788</p>
-                  <p>Atención online y presencial</p>          
-                  <a href="/" className="animate__animated animate__swing btn btn-dark ">Más info</a>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-lg-5 col-md-5 col-sm-6" id="tarjeta" >
-              <div className="card">
-                  <div className="head">
-                    <div className="circle">
-                      <div className="img">
-                        <img className="img-fluid" src="Images/Julieta.png" alt="Lic. Julieta Reynoso"/>
-                      </div>
-                    </div>
-                  </div>
-                <div className="description">
-                  <h3>Lic. Julieta Reynoso</h3>
-                  <p className="fw-bold">PSICÓLOGA</p>
-                  <p>M.P. 12770</p>
-                  <p>Atención online y presencial</p>  
-                  <a href="/" className="animate__animated animate__swing btn btn-dark">Más info</a>
-                </div>
-              </div>
-            </div>
-          </div>
-  
-  */}
  </div>
                 )
             }
